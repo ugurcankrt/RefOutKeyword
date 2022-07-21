16e0e6b31be7a0945dcb72c9893812cc16f853e0
Ayşegül Işık <18010207065@ogrenci.bartin.edu.tr>
2022-07-21 23:02:53 +0300

    Proje dosyası ekle.

---
+++ RefOutKeyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace RefOutKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number1=1;
+            int number2 = 200;
+            var result = Add(ref number1,number2);
+            Console.WriteLine(result);
+            Console.WriteLine(number1);      
+        }
+    
+        static int Add(ref int number1, int number2)
+        {
+
+            number1 = 122;
+            number1 = 11;
+            return number1 + number2;
+        
+        
+        }         
+    
+    }
+}
